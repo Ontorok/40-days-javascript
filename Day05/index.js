e7de8e4e67c71aa396ff864430a8d08a1f8f9c24
Nasir Ahmed <nasir.amath07@gmail.com>
2025-12-11 18:56:29 +0600

    printing star

--- Day05/index.js
+++ Day05/index.js
@@ -1,10 +1,16 @@
 console.log("Day 05");
 
+// for (let i = 1; i <= 5; i++) {
+//   for (let j = 1; j <= 3; j++) {
+//     if (j === 2) {
+//       continue;
+//     }
+//     console.log({ i, j });
+//   }
+// }
+
 for (let i = 1; i <= 5; i++) {
-  for (let j = 1; j <= 3; j++) {
-    if (j === 2) {
-      continue;
-    }
-    console.log({ i, j });
+  for (let star = 1; star <= i; star++) {
+    let starPrint;
   }
 }
