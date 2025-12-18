b99e78a6bcffc8135dac531dd366de161c1f6ed2
Nasir Ahmed <nasir.amath07@gmail.com>
2025-12-18 18:28:37 +0600

    feat: add day 06 task(wip)

---
+++ Day06/index.js
@@ -0,0 +1,11 @@
+console.log("Day 06");
+
+let initValue = 1;
+while (initValue <= 5) {
+  console.log(initValue);
+
+  initValue++;
+}
+
+let doWhile = 1;
+do {} while (doWhile <= 5);
---
+++ Day06/task.js
@@ -0,0 +1,46 @@
+console.log("Day 06");
+
+//#region [1. Write a Function to Convert Celsius to Fahrenheit]
+function convertCelsiusToFahrenheit(tempInCelsius) {
+  let result = (tempInCelsius * 9) / 5 + 32;
+  console.log(`1. ${tempInCelsius} degree celsius is equivalent to ${result} degree fahrenheit`);
+}
+convertCelsiusToFahrenheit(32);
+//#endregion
+
+//#region [2. Create a Function to Find the Maximum of Two Numbers]
+function findMaximum(num1, num2) {
+  let maxNum = num1 > num2 ? num1 : num2;
+  console.log(`2. Maximum of ${num1} and ${num2} is ${maxNum}`);
+}
+findMaximum(-3, -2);
+//#endregion
+
+//#region [3. Function to Check if a String is a Palindrome]
+function checkPalindrome(text = "") {
+  let test = text.split("");
+  console.log(test);
+}
+checkPalindrome("ansir");
+//#endregion
+//#region []
+
+//#endregion
+//#region []
+
+//#endregion
+//#region []
+
+//#endregion
+//#region []
+
+//#endregion
+//#region []
+
+//#endregion
+//#region []
+
+//#endregion
+//#region []
+
+//#endregion
